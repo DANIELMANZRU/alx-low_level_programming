359278a533c3d6bf20616aff2eb7299ffdb68ed6
DANIELMANZRU <dmuiruri200@gmail.com>
2023-08-21 12:44:46 -0700

    bit man.

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *used to  get_edianness - checks if a machine is little or big endian
- * Return: 0 for biger, 1 for little
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
 {
